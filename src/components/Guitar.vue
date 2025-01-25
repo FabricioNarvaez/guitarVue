3a04099f9c47599d411648a2094e01963be53ea4
FabricioNarvaez <narvaezfabricio38@gmail.com>
2025-01-25 14:06:57 +0100

    Shadow on guitar hover added

--- src/components/Guitar.vue
+++ src/components/Guitar.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="col-md-6 col-lg-4 my-4 row align-items-center">
+    <div class="col-md-6 col-lg-4 my-4 row align-items-center guitar">
         <div class="col-4">
             <img class="img-fluid" :src="`/img/${guitar.imagen}.jpg`" :alt="`imagen guitarra ${guitar.nombre}`">
         </div>
@@ -9,15 +9,24 @@
             <p class="fw-black text-primary fs-3">{{ guitar.precio }} €</p>
             <button @click="$emit('agregarCarrito', guitar)" type="button" class="btn btn-dark w-100 ">Agregar al Carrito</button>
         </div>
-    </div><!-- FIN GUITARRA -->
+    </div>
 </template>
 
 <script setup>
-    import { ref } from 'vue';
-
     defineProps({
         guitar: Object
     })
 
     defineEmits(['agregarCarrito']);
 </script>
+
+<style>
+    .guitar {
+        transition: all .5s ease-in-out;
+    }
+    
+    .guitar:hover{
+        box-shadow: 0px 0px 5px 3px rgba(0, 0, 0, 0.2);
+        z-index: 1000;
+    }
+</style>
