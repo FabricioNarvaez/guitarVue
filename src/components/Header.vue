ea8fa785540392b80edbc4577ee8040611096e98
FabricioNarvaez <narvaezfabricio38@gmail.com>
2025-01-27 16:15:56 +0100

    Cart moved to a specific component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,61 +1,7 @@
 <template>
     <header class="py-5 header">
         <div class="container-xl">
-            <div class="row justify-content-center justify-content-md-between">
-                <div class="col-8 col-md-3">
-                    <a href="index.html">
-                        <img class="img-fluid" src="/img/logo.svg" alt="imagen logo">
-                    </a>
-                </div>
-                <nav class="col-md-6 a mt-5 d-flex align-items-start justify-content-end">
-                    <div class="carrito">
-                        <img class="img-fluid" src="/img/carrito.png" alt="imagen carrito" />
-
-                        <div id="carrito" class="bg-white p-3">
-                            <p v-if="!cart.length" class="text-center">El carrito esta vacío</p>
-                            <div v-else>
-                                <table class="w-100 table">
-                                    <thead>
-                                        <tr>
-                                            <th>Imagen</th>
-                                            <th>Nombre</th>
-                                            <th>Precio</th>
-                                            <th>Cantidad</th>
-                                            <th></th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr v-for="cartItem in cart">
-                                            <td>
-                                                <img class="img-fluid" :src="`/img/${cartItem.imagen}.jpg`" :alt="`imagen guitarra ${cartItem.nombre}`">
-                                            </td>
-                                            <td>{{ cartItem.nombre }}</td>
-                                            <td class="fw-bold">{{ cartItem.precio }}</td>
-                                            <td class="flex align-items-start gap-4">
-                                                <button @click="decreaseAmount(cartItem)" type="button" class="btn btn-dark">
-                                                    -
-                                                </button>
-                                                {{ cartItem.amount }}
-                                                <button @click="increaseAmount(cartItem)" type="button" class="btn btn-dark">
-                                                    +
-                                                </button>
-                                            </td>
-                                            <td>
-                                                <button @click="$emit('deleteGuitar', cartItem)" class="btn btn-danger" type="button">
-                                                    X
-                                                </button>
-                                            </td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-
-                                <p class="text-end">Total pagar: <span class="fw-bold">{{ cartTotal }} €</span></p>
-                                <button @click="$emit('emptyCart')" class="btn btn-dark w-100 mt-3 p-2">Vaciar Carrito</button>
-                            </div>
-                        </div>
-                    </div>
-                </nav>
-            </div>
+            <Cart :cart="cart" @deleteGuitar="handleDeleteGuitar" @emptyCart="handleEmptyCart"/>
 
             <PromoGuitar :promoGuitar="promoGuitar" @agregarCarrito="handleAgregarCarrito"/>
         </div>
@@ -65,9 +11,8 @@
 </template>
 
 <script setup>
-    import { computed } from 'vue';
-
     import PromoGuitar from './PromoGuitar.vue';
+    import Cart from './Cart.vue';
 
     const props = defineProps({
         cart: {
@@ -82,11 +27,8 @@
 
     const emit = defineEmits(['agregarCarrito', 'emptyCart', 'deleteGuitar']);
     
-    const cartTotal = computed(() => props.cart.reduce((acc, item) => acc + item.precio * item.amount, 0));
-
-    const increaseAmount = (guitar)=> { guitar.amount++ };
-    const decreaseAmount = (guitar)=> { if(guitar.amount > 0) guitar.amount-- };
+    const handleDeleteGuitar = (guitar)=> {emit('deleteGuitar', guitar)};
     const handleAgregarCarrito = (guitar)=> {emit('agregarCarrito', guitar)};
-    
+    const handleEmptyCart = ()=> {emit('emptyCart')};
     
 </script>
